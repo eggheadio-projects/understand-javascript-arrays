9bfc85716918266abdd0e1f67dd51df1ba8221d1
creeland <cree@provinsal.com>
2017-10-06 12:40:27 -0700

    Add lesson 09 example code

--- script.js
+++ script.js
@@ -1,51 +1,41 @@
 // see console for output!
 
-const lessons = [
+const items = [
     {
-        title: 'Javascript Arrays in Depth - join',
-        views: 960,
-        tags: ['array', 'join']
+        active: true,
+        firstname: 'Shane',
+        lastname: 'Osbourne'
     },
     {
-        title: 'Javascript Arrays in Depth - concat',
-        views: 1050,
-        tags: ['array', 'concat']
+        active: true
+        firstname: 'Sally',
+        lastname: 'Osbourne'
     },
     {
-        title: 'Javascript Arrays in Depth - slice',
-        views: 2503,
-        tags: ['array', 'slice']
-    },
-    {
-        title: 'Javascript Functions in Depth - bind',
-        views: 2500,
-        tags: ['functions', 'bind']
+        active: false,
+        firstname: 'Ben',
+        lastname: 'Barker'
     }
 ];
 
-const minViews = 1000;
-const searchTerm = 'array';
+const mapped = items
+    .filter(x => x.active)
+    .map(x => x.firstname);
 
-const filtered = lessons
-    .filter(x => x.tags.indexOf(searchTerm) > -1)
-    .filter(x => x.views > minViews)
-    .sort((a, b) => b.views - a.views)
-    .map(x => `  <li>${x.title}</li>`)
-    .join('\n');
+console.log(createHtmlList(mapped)) || displayInPreview(createHtmlList(mapped))
 
-console.log(`<ul>
-${filtered}
-</ul>`) || displayInPreview(`<ul>
-${filtered}
-</ul>`);
+function createHtmlList(items) {
+    const listElements = items.map(x => `<li>${x.firstname}</li>`)
+    return `<ul>\n\t${listElements}\n</ul>`;
+}
 
 
 
 
 // display in plunker preview
 function displayInPreview(string) {
-  var newDiv = document.createElement("div"); 
-  var newContent = document.createTextNode(string); 
+  var newDiv = document.createElement("div");
+  var newContent = document.createTextNode(string);
   newDiv.appendChild(newContent);
   document.body.appendChild(newDiv)
 }
